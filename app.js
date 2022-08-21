f7f36d44de28ebcffb70713b24433d464736150f
Hammad Godil <hammadgodil@gmail.com>
2022-08-21 11:46:10 +0500

    completing assignment

--- app.js
+++ app.js
@@ -8,32 +8,24 @@ console.log(Number(reverse_a_number(32243)));
 
 
 // Checking a Palindrome or not? 
-
 function check_Palindrome(str_entry){
-	// Change the string into lower case and remove  all non-alphanumeric characters
 	   var cstr = str_entry.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
 		var ccount = 0;
-	// Check whether the string is empty or not
 		if(cstr==="") {
 			console.log("Nothing found!");
 			return false;
 		}
-	// Check if the length of the string is even or odd 
 		if ((cstr.length) % 2 === 0) {
 			ccount = (cstr.length) / 2;
 		} else {
-	// If the length of the string is 1 then it becomes a palindrome
 			if (cstr.length === 1) {
 				console.log("Entry is a palindrome.");
 				return true;
 			} else {
-	// If the length of the string is odd ignore middle character
 				ccount = (cstr.length - 1) / 2;
 			}
 		}
-	// Loop through to check the first character to the last character and then move next
 		for (var x = 0; x < ccount; x++) {
-	// Compare characters and drop them if they do not match 
 			if (cstr[x] != cstr.slice(-1-x)[0]) {
 				console.log("Entry is not a palindrome.");
 				return false;
@@ -95,4 +87,110 @@ function uppercase(str)
   return newarray1.join(' ');
 }
 console.log(uppercase("the quick brown fox"));
+
+// Finding longest word in the string
+function find_longest_word(str)
+{
+  var array1 = str.match(/\w[a-z]{0,}/gi);
+  var result = array1[0];
+
+  for(var x = 1 ; x < array1.length ; x++)
+  {
+    if(result.length < array1[x].length)
+    {
+    result = array1[x];
+    } 
+  }
+  return result;
+}
+console.log(find_longest_word('Web Development Tutorial'));
+
+// Counting Vowel in the string
+function vowel_count(str1)
+{
+  var vowel_list = 'aeiouAEIOU';
+  var vcount = 0;
+  
+  for(var x = 0; x < str1.length ; x++)
+  {
+    if (vowel_list.indexOf(str1[x]) !== -1)
+    {
+      vcount += 1;
+    }
+  
+  }
+  return vcount;
+}
+console.log(vowel_count("The quick brown fox"));
+
+// checking a number is prime or not
+function test_prime(n)
+{
+
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+}
+
+console.log(test_prime(37));
+
+// accepting argument & returning type
+function detect_data_type(value)
+{
+var dtypes = [Function, RegExp, Number, String, Boolean, Object], x, len;
+    
+if (typeof value === "object" || typeof value === "function") 
+    {
+     for (x = 0, len = dtypes.length; x < len; x++) 
+     {
+            if (value instanceof dtypes[x])
+            {
+                return dtypes[x];
+            }
+      }
+    }
+    
+    return typeof value;
+}
+console.log(detect_data_type(12));
+console.log(detect_data_type('w3resource'));
+console.log(detect_data_type(false));
+
+// finding lowerst & second greatest numbers
+function Second_Greatest_Lowest(arr_num)
+{
+  arr_num.sort(function(x,y)
+           {
+           return x-y;
+           });
+  var uniqa = [arr_num[0]];
+  var result = [];
+  
+  for(var j=1; j < arr_num.length; j++)
+  {
+    if(arr_num[j-1] !== arr_num[j])
+    {
+      uniqa.push(arr_num[j]);
+    }
+  }
+    result.push(uniqa[1],uniqa[uniqa.length-2]);
+  return result.join(',');
+  }
+
+console.log(Second_Greatest_Lowest([1,2,3,4,5]));
 
